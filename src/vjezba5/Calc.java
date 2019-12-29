17fdca288da629d5aa5dabc75f0789f2fea584fc
enes <mesa@DESKTOP-6T2EFT8>
2019-12-29 13:49:24 +0100

    i12345678 vjezba 5
    = Vjezba6 klasa Calc

---
+++ src/vjezba5/Calc.java
@@ -0,0 +1,15 @@
+package vjezba5;
+
+public class Calc {
+
+    
+   public static double add(double a, double b){
+       return a+b;
+   } 
+    
+    public static double add(double a, double b, double c)
+    {
+       return a + b + c;
+    }
+    
+}
---
+++ src/vjezba5/Vjezba5.java
@@ -0,0 +1,18 @@
+package vjezba5;
+
+public class Vjezba5 {
+
+    public static void main(String[] args) {
+
+        
+        double res1 = Calc.add(4, 6);
+        double res2 = Calc.add(4, 6, 8);
+        System.out.println(res1);
+        System.out.println(res2);
+        
+        
+        
+        
+    }
+
+}
